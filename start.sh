b25e6e36beb612e76d419ed9d235b76adbdbea34
Skzbot <145977507+Skzbot@users.noreply.github.com>
2023-10-01 18:43:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Skzbot/PremiumFilter /PiroAutoFilterBot
+  git clone https://github.com/Skzbot/PremiumFilter /PremiumFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /PremiumFilter
 fi
-cd /PiroAutoFilterBot
+cd /PremiumFilter
 pip3 install -U -r requirements.txt
 echo "Starting PiroAutoFilterBot...."
 python3 bot.py
